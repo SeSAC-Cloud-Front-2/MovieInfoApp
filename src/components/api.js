298a0f2e9964510c141f08807e9356a559376e8c
jaeminLee524 <44490394+jaeminLee524@users.noreply.github.com>
2021-12-17 09:39:03 +0900

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
 //TMDB api를 상수로 선언
-export const API_URL = <<URL>>
+export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = <<API_KEY>>
-export const IMAGE_URL = <<IMAGE_URL>>     
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
