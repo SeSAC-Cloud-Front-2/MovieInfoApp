995a43883a7169f8e343776eba5ba1e55e049109
SeSAC-Cloud-CTW <slowlight50@gmail.com>
2021-12-21 13:31:18 +0900

    정렬된 상태에서도 검색 가능하도록 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,192 +1,186 @@
-import React, { useEffect, useState, useRef } from 'react';
-import styled from 'styled-components';
-import Search from './Search';
+import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
+import Search from "./Search";
 
 const HeaderBlock = styled.div`
-    margin: 0 auto;
-    z-index: 2;
-    width: 100%;
-    height: 60px;
-    display: flex;
-    position: fixed;
-    justify-content: center;
-    align-items: center;
-    backdrop-filter: blur(0px);
-    background-color: rgb(21, 21, 21, 0.55);
-    border-bottom-color: rbg(35, 35, 35, 0.2);
-    border: 1px solid black;
-    box-sizing: border-box;
-    color: rgb(250, 250, 250);
+  margin: 0 auto;
+  z-index: 2;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(0px);
+  background-color: rgb(21, 21, 21, 0.55);
+  border-bottom-color: rbg(35, 35, 35, 0.2);
+  border: 1px solid black;
+  box-sizing: border-box;
+  color: rgb(250, 250, 250);
 `;
 
 const NavBlock = styled.ul`
-    display: flex;
-    list-style: none;
+  display: flex;
+  list-style: none;
 
-    li {
-        display: inline-block;
-        margin: 0px 50px;
-        /* padding: 0px 25px; */
-        box-sizing: border-box;
-        color: rgb(230, 230, 230);
-        cursor: pointer;
-        font-size: 18px;
-        font-weight: 500;
-        text-align: center;
-        line-height: 60px;
-    }
+  li {
+    display: inline-block;
+    margin: 0px 50px;
+    /* padding: 0px 25px; */
+    box-sizing: border-box;
+    color: rgb(230, 230, 230);
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+    line-height: 60px;
+  }
 `;
 
 const DropDown = styled.li`
-    display: relative;
+  display: relative;
 
-    :hover {
-        div {
-            display: block;
-        }
+  :hover {
+    div {
+      display: block;
     }
+  }
 `;
 
 const DropDownContent = styled.div`
-    display: none;
-    position: absolute;
-    top: 55px;
-    width: auto;
-    /* border: 1px solid red; */
-    background-color: #5c5c5c;
-    ul {
-        width: inherit;
-        padding: 0;
-    }
-    li {
-        display: block;
-        width: inherit;
-        margin: 0;
-        padding: 0;
-    }
-    ul > li:hover {
-        background: #212529;
-        transition: ease 0.5s;
-    }
+  display: none;
+  position: absolute;
+  top: 55px;
+  width: auto;
+  /* border: 1px solid red; */
+  background-color: #5c5c5c;
+  ul {
+    width: inherit;
+    padding: 0;
+  }
+  li {
+    display: block;
+    width: inherit;
+    margin: 0;
+    padding: 0;
+  }
+  ul > li:hover {
+    background: #212529;
+    transition: ease 0.5s;
+  }
 `;
 
 function Header({
-    containerRef,
-    nameChange,
-    setFilterFlags,
-    filter,
-    movies,
-    setMoviesCopy,
-    reset,
+  containerRef,
+  nameChange,
+  filter,
+  movies,
+  moviesCopy,
+  setMoviesCopy,
+  setFilteredMovies,
 }) {
-    // GridCards 위치(위) 이동 함수
-    const goUp = () => {
-        containerRef.current.scrollTo(0, 0);
-    };
+  // GridCards 위치(위) 이동 함수
+  const goUp = () => {
+    containerRef.current.scrollTo(0, 0);
+  };
 
-    const handleClick = e => {
-        const id = e.target.id;
+  const handleClick = (e) => {
+    const id = e.target.id;
 
-        // text flag 역할
+    // text flag 역할
+    nameChange(id);
+    filter(id);
+
+    // 문구 변경을 위한 flag
+
+    // 스크린의 가장 위로
+    window.scrollTo(0, 0);
+
+    // GridCards 위치 위로
+    goUp();
+  };
+
+  const handleDropDownClick = (e) => {
+    const id = e.target.id;
+    filter(id);
+
+    // 스크린의 가장 위로
+    window.scrollTo(0, 0);
+
+    // GridCards 위치 위로
+    goUp();
+
+    // text flag 역할
+    switch (id) {
+      case "28":
+        nameChange(3);
+        break;
+      case "12":
+        nameChange(4);
+        break;
+      case "16":
+        nameChange(5);
+        break;
+      case "35":
+        nameChange(6);
+        break;
+      case "80":
+        nameChange(7);
+        break;
+      default:
         nameChange(id);
+        break;
+    }
+  };
 
-        const newFilter = new Array(4);
-        newFilter.fill(false);
-
-        newFilter[id] = true;
-
-        setFilterFlags(newFilter);
-        filter(id);
-
-        // 문구 변경을 위한 flag
-
-        // 스크린의 가장 위로
-        window.scrollTo(0, 0);
-
-        // GridCards 위치 위로
-        goUp();
-    };
-
-    const handleDropDownClick = e => {
-        const id = e.target.id;
-        filter(id);
-
-        // 스크린의 가장 위로
-        window.scrollTo(0, 0);
-
-        // GridCards 위치 위로
-        goUp();
-
-        // text flag 역할
-        switch (id) {
-            case '28':
-                nameChange(3);
-                break;
-            case '12':
-                nameChange(4);
-                break;
-            case '16':
-                nameChange(5);
-                break;
-            case '35':
-                nameChange(6);
-                break;
-            case '80':
-                nameChange(7);
-                break;
-            default:
-                nameChange(id);
-                break;
-        }
-    };
-
-    return (
-        <HeaderBlock>
-            <NavBlock>
-                <li id="0" onClick={handleClick}>
-                    홈
-                </li>
-                <li id="1" onClick={handleClick}>
-                    인기
-                </li>
-                <li id="2" onClick={handleClick}>
-                    최신
-                </li>
-                <DropDown>
-                    장르
-                    <DropDownContent>
-                        <ul>
-                            <li id="28" onClick={handleDropDownClick}>
-                                {/* Action */}
-                                액션영화
-                            </li>
-                            <li id="12" onClick={handleDropDownClick}>
-                                {/* Adventure */}
-                                모험영화
-                            </li>
-                            <li id="16" onClick={handleDropDownClick}>
-                                {/* Animation */}
-                                애니메이션영화
-                            </li>
-                            <li id="35" onClick={handleDropDownClick}>
-                                {/* Comedy */}
-                                코미디영화
-                            </li>
-                            <li id="80" onClick={handleDropDownClick}>
-                                {/* Crime */}
-                                범죄영화
-                            </li>
-                        </ul>
-                    </DropDownContent>
-                </DropDown>
-            </NavBlock>
-            <Search
-                movies={movies}
-                setMoviesCopy={setMoviesCopy}
-                reset={reset}
-            />
-        </HeaderBlock>
-    );
+  return (
+    <HeaderBlock>
+      <NavBlock>
+        <li id="0" onClick={handleClick}>
+          홈
+        </li>
+        <li id="1" onClick={handleClick}>
+          인기
+        </li>
+        <li id="2" onClick={handleClick}>
+          최신
+        </li>
+        <DropDown>
+          장르
+          <DropDownContent>
+            <ul>
+              <li id="28" onClick={handleDropDownClick}>
+                {/* Action */}
+                액션영화
+              </li>
+              <li id="12" onClick={handleDropDownClick}>
+                {/* Adventure */}
+                모험영화
+              </li>
+              <li id="16" onClick={handleDropDownClick}>
+                {/* Animation */}
+                애니메이션영화
+              </li>
+              <li id="35" onClick={handleDropDownClick}>
+                {/* Comedy */}
+                코미디영화
+              </li>
+              <li id="80" onClick={handleDropDownClick}>
+                {/* Crime */}
+                범죄영화
+              </li>
+            </ul>
+          </DropDownContent>
+        </DropDown>
+      </NavBlock>
+      <Search
+        movies={movies}
+        moviesCopy={moviesCopy}
+        setMoviesCopy={setMoviesCopy}
+        setFilteredMovies={setFilteredMovies}
+      />
+    </HeaderBlock>
+  );
 }
 
 export default Header;
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,244 +1,247 @@
-import React, { useCallback, useEffect, useState, useRef } from 'react';
-import { API_URL, API_KEY, IMAGE_URL } from './api';
-import MainImage from './MainImage';
-import GridCards from './GridCards';
-import styled from 'styled-components';
-import Header from './Header';
-import TopButton from './TopButton';
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import { API_URL, API_KEY, IMAGE_URL } from "./api";
+import MainImage from "./MainImage";
+import GridCards from "./GridCards";
+import styled from "styled-components";
+import Header from "./Header";
+import TopButton from "./TopButton";
 
 const MainPageBlock = styled.div`
-    width: 100%;
-    margin: 0px;
+  width: 100%;
+  margin: 0px;
 `;
 
 const MovieCardBlock = styled.div`
-    width: 90%;
-    /* height: ; */
-    margin: 1rem auto;
-    .text {
-        color: white;
-    }
-    box-sizing: border-box;
+  width: 90%;
+  /* height: ; */
+  margin: 1rem auto;
+  .text {
+    color: white;
+  }
+  box-sizing: border-box;
 `;
 
 const MovieCardBody = styled.div`
-    height: 100vh;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin: 0 auto;
-    width: 90%;
-    box-sizing: border-box;
-    /* 이미지가 div영역 밖으로 나가는거 방지 */
-    overflow: auto;
-    /* overflow의 스크롤바 없애고 기능은 살림*/
-    -ms-overflow-style: none;
-    ::-webkit-scrollbar {
-        display: none;
-    }
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+  width: 90%;
+  box-sizing: border-box;
+  /* 이미지가 div영역 밖으로 나가는거 방지 */
+  overflow: auto;
+  /* overflow의 스크롤바 없애고 기능은 살림*/
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const ButtonBlock = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const Button = styled.button`
-    width: 104px;
-    height: 34px;
-    background-color: rgba(0, 0, 0, 0.2);
-    color: rgba(250, 250, 250, 0.9);
-    border: 1px solid rgba(255, 255, 255, 0.3);
-    border-radius: 4px;
-    outline: none;
-    &:hover {
-        cursor: pointer;
-    }
-    &:active {
-        box-shadow: 1px 1px 0 rgb(0, 0, 0, 0.5);
-        position: relative;
-        top: 2px;
-    }
+  width: 104px;
+  height: 34px;
+  background-color: rgba(0, 0, 0, 0.2);
+  color: rgba(250, 250, 250, 0.9);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  border-radius: 4px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    box-shadow: 1px 1px 0 rgb(0, 0, 0, 0.5);
+    position: relative;
+    top: 2px;
+  }
 `;
 
 function MainPage() {
-    const [movies, setMovies] = useState([]);
-    const [moviesCopy, setMoviesCopy] = useState(movies.concat());
-    const [filterFlags, setFilterFlags] = useState(
-        //home, popular, latest, genre
-        [true, false, false, false]
-    );
-    // mainMovieImage : 0번째 인덱스의 배열
-    const [mainMovieImage, setMainMovieImage] = useState(null);
-    const [page, setPage] = useState(20);
-    const [number, setNumber] = useState(0);
-    const name = [
-        '인기',
-        '인기',
-        '최신',
-        '액션',
-        '모험',
-        '애니메이션',
-        '코미디',
-        '범죄',
-        // 'Action',
-        // 'Adventure',
-        // 'Animation',
-        // 'Comedy',
-        // 'Crime',
-    ];
-    // ref를 넘겨줘 current로 접근하기 위해
-    const containerRef = useRef();
+  const [movies, setMovies] = useState([]);
+  const [moviesCopy, setMoviesCopy] = useState(movies.concat());
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  // mainMovieImage : 0번째 인덱스의 배열
+  const [mainMovieImage, setMainMovieImage] = useState(null);
+  const [page, setPage] = useState(20);
+  const [number, setNumber] = useState(0);
+  const name = [
+    "인기",
+    "인기",
+    "최신",
+    "액션",
+    "모험",
+    "애니메이션",
+    "코미디",
+    "범죄",
+    // 'Action',
+    // 'Adventure',
+    // 'Animation',
+    // 'Comedy',
+    // 'Crime',
+  ];
+  // ref를 넘겨줘 current로 접근하기 위해
+  const containerRef = useRef();
 
-    // 최초 브라우저 redering되었을 때 실행
-    useEffect(() => {
-        const response = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=`;
-        callMovie(response);
-        // getMovie(response);
-    }, []);
+  // 최초 브라우저 redering되었을 때 실행
+  useEffect(() => {
+    const response = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=`;
+    callMovie(response);
+    // getMovie(response);
+  }, []);
 
-    const callMovie = async response => {
-        let movieList = [];
-        for (let i = 1; i < 11; i++) {
-            movieList = movieList.concat(
-                await fetch(response + i)
-                    .then(response => response.json())
-                    .then(response => response.results)
-            );
-        }
-        setMovies(movieList.concat());
-        setMainMovieImage(movieList[0]);
-    };
+  const callMovie = async (response) => {
+    let movieList = [];
+    for (let i = 1; i < 11; i++) {
+      movieList = movieList.concat(
+        await fetch(response + i)
+          .then((response) => response.json())
+          .then((response) => response.results)
+      );
+    }
+    setMovies(movieList.concat());
+    setMainMovieImage(movieList[0]);
+  };
 
-    // const getMovie = async response => {
-    //     try {
-    //         let movieList = [];
-    //         for (let i = 1; i < 11; i++) {
-    //             const res = await axios.get(response + i);
-    //             console.log(res);
-    //             await movieList.concat(res.data.results);
-    //           }
-    //           console.log(movieList);
-    //         setMovies(movieList.concat());
-    //         setMainMovieImage(movieList[0]);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // };
+  // const getMovie = async response => {
+  //     try {
+  //         let movieList = [];
+  //         for (let i = 1; i < 11; i++) {
+  //             const res = await axios.get(response + i);
+  //             console.log(res);
+  //             await movieList.concat(res.data.results);
+  //           }
+  //           console.log(movieList);
+  //         setMovies(movieList.concat());
+  //         setMainMovieImage(movieList[0]);
+  //     } catch (e) {
+  //         console.log(e);
+  //     }
+  // };
 
-    useEffect(() => {
-        setMoviesCopy(movies.concat());
-    }, [movies]);
+  useEffect(() => {
+    setMoviesCopy(movies.concat());
+    setFilteredMovies(movies.concat());
+  }, [movies]);
 
-    const onClick = () => {
-        setPage(page + 20);
-    };
+  const onClick = () => {
+    setPage(page + 20);
+  };
 
-    const filter = useCallback(
-        id => {
-            const nextMovies = movies.concat();
-            setPage(20); // 최대 표시값 초기화
-            if (id === '0') {
-                //home
-                reset();
-            } else if (id === '1') {
-                //popular
-                setMoviesCopy(
-                    nextMovies.sort((a, b) =>
-                        a.popularity > b.popularity ? -1 : 1
-                    )
-                );
-            } else if (id === '2') {
-                //new
-                setMoviesCopy(
-                    nextMovies.sort((a, b) =>
-                        new Date(a.release_date) > new Date(b.release_date)
-                            ? -1
-                            : 1
-                    )
-                );
-            } else {
-                const genreNo = parseInt(id);
-                setMoviesCopy(
-                    nextMovies.filter(movie =>
-                        movie.genre_ids.includes(genreNo)
-                    )
-                );
-            }
-        },
-        [moviesCopy]
-    );
+  const filter = useCallback(
+    (id) => {
+      const nextMovies = movies.concat();
+      setPage(20); // 최대 표시값 초기화
+      if (id === "0") {
+        //home
+        reset();
+      } else if (id === "1") {
+        //popular
+        setMoviesCopy(
+          nextMovies.sort((a, b) => (a.popularity > b.popularity ? -1 : 1))
+        );
+        setFilteredMovies(
+          nextMovies.sort((a, b) => (a.popularity > b.popularity ? -1 : 1))
+        );
+      } else if (id === "2") {
+        //new
+        setMoviesCopy(
+          nextMovies.sort((a, b) =>
+            new Date(a.release_date) > new Date(b.release_date) ? -1 : 1
+          )
+        );
+        setFilteredMovies(
+          nextMovies.sort((a, b) =>
+            new Date(a.release_date) > new Date(b.release_date) ? -1 : 1
+          )
+        );
+      } else {
+        const genreNo = parseInt(id);
+        setMoviesCopy(
+          nextMovies.filter((movie) => movie.genre_ids.includes(genreNo))
+        );
+        setFilteredMovies(
+          nextMovies.filter((movie) => movie.genre_ids.includes(genreNo))
+        );
+      }
+    },
+    [moviesCopy]
+  );
 
-    const reset = () => {
-        setMoviesCopy(movies.concat());
-    };
+  const reset = () => {
+    setMoviesCopy(movies.concat());
+    setFilteredMovies(movies.concat());
+  };
 
-    // header 컨텐츠 클릭에 따른 id value setting
-    const nameChange = number => {
-        setNumber(number);
-    };
-    return (
-        <MainPageBlock>
-            <Header
-                containerRef={containerRef}
-                nameChange={nameChange}
-                filterFlags={filterFlags}
-                setFilterFlags={setFilterFlags}
-                filter={filter}
-                movies={movies}
-                moviesCopy={moviesCopy}
-                setMoviesCopy={setMoviesCopy}
-                reset={reset}
-            />
-            {/* Main Image */}
-            {/* image는 url을 갖고있음 */}
-            {mainMovieImage && (
-                <MainImage
-                    image={`${IMAGE_URL}w1280${mainMovieImage.backdrop_path}`}
-                    title={mainMovieImage.original_title}
-                    overview={mainMovieImage.overview}
-                />
+  // header 컨텐츠 클릭에 따른 id value setting
+  const nameChange = (number) => {
+    setNumber(number);
+  };
+  return (
+    <MainPageBlock>
+      <Header
+        containerRef={containerRef}
+        nameChange={nameChange}
+        filter={filter}
+        movies={movies}
+        moviesCopy={moviesCopy}
+        setMoviesCopy={setMoviesCopy}
+        setFilteredMovies={setFilteredMovies}
+        reset={reset}
+      />
+      {/* Main Image */}
+      {/* image는 url을 갖고있음 */}
+      {mainMovieImage && (
+        <MainImage
+          image={`${IMAGE_URL}w1280${mainMovieImage.backdrop_path}`}
+          title={mainMovieImage.original_title}
+          overview={mainMovieImage.overview}
+        />
+      )}
+      {/* <MovieCardBlock> */}
+      <MovieCardBlock>
+        <h2 className="text">{`${name[number]}영화`}</h2>
+        {/* Movie Grid Cards */}
+        <MovieCardBody ref={containerRef}>
+          {/* <MovieCardBody> */}
+          {filteredMovies &&
+            filteredMovies.map((movie, index) =>
+              index < page ? (
+                <div key={index}>
+                  <GridCards
+                    posterPath={
+                      movie.poster_path
+                        ? `${IMAGE_URL}w500${movie.poster_path}`
+                        : null
+                    }
+                    movie={movie}
+                    modalPosterPath={
+                      movie.poster_path
+                        ? `${IMAGE_URL}original${movie.poster_path}`
+                        : null
+                    }
+                  />
+                </div>
+              ) : (
+                ""
+              )
             )}
-            {/* <MovieCardBlock> */}
-            <MovieCardBlock>
-                <h2 className="text">{`${name[number]}영화`}</h2>
-                {/* Movie Grid Cards */}
-                <MovieCardBody ref={containerRef}>
-                    {/* <MovieCardBody> */}
-                    {moviesCopy &&
-                        moviesCopy.map((movie, index) =>
-                            index < page ? (
-                                <div key={index}>
-                                    <GridCards
-                                        posterPath={
-                                            movie.poster_path
-                                                ? `${IMAGE_URL}w500${movie.poster_path}`
-                                                : null
-                                        }
-                                        movie={movie}
-                                        modalPosterPath={
-                                            movie.poster_path
-                                                ? `${IMAGE_URL}original${movie.poster_path}`
-                                                : null
-                                        }
-                                    />
-                                </div>
-                            ) : (
-                                ''
-                            )
-                        )}
-                </MovieCardBody>
-            </MovieCardBlock>
-            {page < moviesCopy.length + 1 ? (
-                <ButtonBlock>
-                    <Button onClick={onClick}>더보기</Button>
-                </ButtonBlock>
-            ) : (
-                ''
-            )}
-            <TopButton containerRef={containerRef}></TopButton>
-        </MainPageBlock>
-    );
+        </MovieCardBody>
+      </MovieCardBlock>
+      {page < filteredMovies.length + 1 ? (
+        <ButtonBlock>
+          <Button onClick={onClick}>더보기</Button>
+        </ButtonBlock>
+      ) : (
+        ""
+      )}
+      <TopButton containerRef={containerRef}></TopButton>
+    </MainPageBlock>
+  );
 }
 
 export default MainPage;
