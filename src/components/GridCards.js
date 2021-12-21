53b85d49b0a58655a230282d5030ef5596ff89ec
TheSunBeneathTheSea <80350207+TheSunBeneathTheSea@users.noreply.github.com>
2021-12-21 10:12:21 +0900

    출연 정보를 받아오는 함수 작성

--- src/components/GridCards.js
+++ src/components/GridCards.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
+import { API_KEY } from "./api";
 import Modal from './Modal';
 
 function GridCards({ posterPath, movie, modalPosterPath }) {
   //modal on/off
   const [openModal, setOpenModal] = useState(false);
+  const [actors, setActors] = useState([]);
   const handleModal = () => {
+    if (!openModal) {
+      getActors();
+    }
     setOpenModal(!openModal);
+  };
+  
+  const getActors = async () => {
+    let actorsList = [];
+
+    const request = `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=${API_KEY}`;
+
+    actorsList = actorsList.concat(
+      await fetch(request)
+        .then((response) => response.json())
+        .then((response) => response)
+    );
+
+    actorsList = actorsList[0].cast
+      .filter((actor) => actor.order < 3)
+      .map((actor) => actor.name);
+
+    let actorsInfo = "";
+
+    actorsList.forEach((actor, index) => {
+      if (index !== actorsList.length - 1) {
+        actorsInfo += actor + ", ";
+      } else {
+        actorsInfo += actor + " ...";
+      }
+    });
+
+    setActors(actorsInfo);
   };
 
   return (
